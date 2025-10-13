d6eb5316695cfe84568a63fbf4abe99ef7271533
Aaron Santana <santanavaldelomaraa@gmail.com>
2025-10-12 20:34:04 -0600

    feat: initialize lab2 project with Express and necessary middleware
    - Added package.json with dependencies including express, body-parser, multer, morgan, and nodemon.
    - Created src/index.js to set up the Express server and middleware for handling requests.
    - Introduced routes for students in src/routes/students.routes.js.
    - Configured environment variables in src/utils/env/envVariables.js to manage the server port.

---
+++ lab2/src/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+import multer from "multer";
+import { PORT } from "./utils/env/envVariables.js";
+import morgan from "morgan";
+
+const app = express();
+
+// Get __dirname equivalent in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const STATIC_DIR = path.join(__dirname, "views");
+
+// ==================== MIDDLEWARES ====================
+app.use(multer().any()); // Handle multipart/form-data
+app.use(express.static(STATIC_DIR)); // Serve static files
+app.use(bodyParser.json()); // Parse application/json
+app.use(bodyParser.urlencoded({ extended: true })); // Parse application/x-www-form-urlencoded
+app.use(morgan('dev')); // Log HTTP requests
+
+
+// ==================== ROUTES ====================
+
+
+
+// ==================== START SERVER ====================
+app.listen(PORT, function () {
+  console.log("Corriendo en http://localhost:" + PORT);
+});
